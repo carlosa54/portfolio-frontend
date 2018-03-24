090176d55731a9b770600e9c79bde773e3375a15
Carlos Ramirez <carlosarg54@gmail.com>
2018-03-24 15:02:50 -0400

    Add api

---
+++ src/utils/api.tsx
@@ -0,0 +1,26 @@
+import { create } from 'apisauce';
+
+const api = create({
+    baseURL: 'http://127.0.0.1:8000/api',
+    headers: { 'Content-Type': 'application/json' }
+});
+
+export function fetchProjects() {
+    return api.get('/projects/?ordering=pk');
+}
+
+export function getProjectDetail(id) {
+    return api.get(`/projects/${id}`);
+}
+
+export function getSkills() {
+    return api.get('/skills/');
+}
+
+export function getWorks() {
+    return api.get('/works/');
+}
+
+export function getWorkDetail(id) {
+    return api.get(`/works/${id} `);
+}
